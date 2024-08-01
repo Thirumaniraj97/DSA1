a5da93399bb113cc38ecc3bec78aee88cdafe2ef
ThiruRR <thirumaniraj_rr@trimble.com>
2024-08-01 18:00:45 +0530

    Sorting Algorithms

---
+++ src/SortingAlgorithm/InsertionSort.java
@@ -0,0 +1,32 @@
+package SortingAlgorithm;
+
+public class InsertionSort {
+
+	public void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public void sort(int[] arr) {
+		int n = arr.length;
+		for (int i = 1; i < n; i++) {
+			int temp = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = temp;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[]{5, 1, 2, 9, 10};
+		InsertionSort is = new InsertionSort();
+		is.printArray(arr);
+		is.sort(arr);
+		is.printArray(arr);
+	}
+}
